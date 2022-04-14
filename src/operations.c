01c475ecb41d7308fa97f3faac8379e1809a7fb3
topheruk <kristopherab@gmail.com>
2022-04-14 22:48:32 +0000

    added files

---
+++ src/operations.c
@@ -0,0 +1,9 @@
+#include "operations.h"
+
+double sum(double a, double b) { return a + b; }
+
+double sub(double a, double b) { return a - b; }
+
+double mult(double a, double b) { return a * b; }
+
+double div(double a, double b) { return a / b; }
