a0d91d2d79a61cc81b850b3aabd4a1225c295bf3
topheruk <kristopherab@gmail.com>
2022-04-14 22:42:40 +0000

    delete sm

--- src/operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERATIONS_H
-#define OPERATIONS_H
-
-double sum(double a, double b);
-double sub(double a, double b);
-double mult(double a, double b);
-double div(double a, double b);
-
-#endif  // OPERATIONS_H
